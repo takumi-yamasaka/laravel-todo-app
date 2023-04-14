badef11a4168dfeb72e4de2e67384bca9d60aba6
takumi-yamasaka <takumi.yamasaka@divx.co.jp>
2023-04-14 12:25:50 +0900

    こみっと

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
 <body class="bg-gray-100">
     <header class="bg-blue-500 py-4">
         <nav class="container mx-auto flex justify-between items-center">
-            <a href="/" class="text-white font-bold text-xl">問題集アプリ</a>
+            <a href="/" class="header-font">問題集アプリ</a>
             <div>
                 @guest
                     <a href="{{ route('login') }}" class="text-white mr-4">ログイン</a>
@@ -24,7 +24,7 @@
                 @else
                     <a href="{{ route('logout') }}"
                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                       class="text-white">
+                       class="header-font">
                         ログアウト
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
--- resources/views/questions/create-gpt.blade.php
+++ resources/views/questions/create-gpt.blade.php
@@ -1,21 +1,38 @@
 @extends('layout')
 
 @section('content')
-    <h1>問題登録・チャットGPTモード</h1>
-    <h2>GPTに以下のプロンプトで質問してみてください。（コピペでいいです）</h2>
-    <p>AWSの勉強をしています。以下の問題の形式でタイトル、問題文、選択肢、回答も含めて出題してください。</p>
-    <p>内容はランダムで構いません。{ "title": "", "question": "", "choices": [ "1. ", "2. ", "3. ", "4. ], "correct_answer_index": "" }</p>
-
-
-    <form action="{{ route('questions.store-gpt') }}" method="POST">
-        @csrf
-        <div>
-            <label for="gpt-generated-json">GPTで生成されたJSON:</label>
-            <textarea id="gpt-generated-json" name="gpt-generated-json" rows="10" cols="50" required></textarea>
+    <div class="container mx-auto mt-10">
+        <h1 class="text-2xl mb-6">問題登録・チャットGPTモード</h1>
+        <h2 class="text-xl mb-4">GPTに以下のプロンプトで質問してみてください。</h2>
+        <div class="bg-gray-100 border border-gray-200 p-4 mb-8 rounded">
+            <p>AWSの勉強をしています。以下の問題の形式でタイトル、問題文、選択肢、回答も含めて出題してください。</p>
+            <p>内容はランダムで構いません。{ "title": "", "question": "", "choices": [ "1. ", "2. ", "3. ", "4. ], "correct_answer_index": "" }</p>
+            <button id="copy-prompt" class="bg-blue-500 text-white px-4 py-2 rounded">コピー</button>
         </div>
 
-        <button type="submit">問題を登録する</button>
-    </form>
+        <form action="{{ route('questions.store-gpt') }}" method="POST" class="bg-white p-8 rounded border border-gray-200">
+            @csrf
+            <div class="mb-4">
+                <label for="gpt-generated-json" class="block text-sm mb-2">GPTで生成されたJSON:</label>
+                <textarea id="gpt-generated-json" name="gpt-generated-json" rows="10" cols="50" class="w-full p-2 border border-gray-200 rounded" required></textarea>
+            </div>
 
-    <a href="{{ route('questions.index') }}">一覧へ戻る</a>
+            <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded">問題を登録する</button>
+        </form>
+
+        <a href="{{ route('questions.index') }}" class="block mt-4 text-blue-500">一覧へ戻る</a>
+    </div>
+
+    <script>
+        document.getElementById('copy-prompt').addEventListener('click', function() {
+            const text = 'AWSの勉強をしています。以下の問題の形式でタイトル、問題文、選択肢、回答も含めて出題してください。\n\n内容はランダムで構いません。{ "title": "", "question": "", "choices": [ "1. ", "2. ", "3. ", "4. ], "correct_answer_index": "" }';
+            const textarea = document.createElement('textarea');
+            textarea.textContent = text;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textarea);
+            alert('コピーしました');
+        });
+    </script>
 @endsection
